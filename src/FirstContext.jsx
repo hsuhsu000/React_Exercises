02dc441eb32c26a28f5182e2fb944b5c80e29edb
HsuShweSinOo <hsushwesinoo2002@gmail.com>
2024-03-19 15:11:36 +0630

    add and minus number using context hook

--- src/FirstContext.jsx
+++ src/FirstContext.jsx
@@ -3,7 +3,8 @@ export const FirstContext = createContext(null);
 const FirstContextProvider = (props) => {
   const userName = "HsuShweSinOo";
   const address = "Yangon";
-  return <FirstContext.Provider value={{ userName, address }}>{props.children}</FirstContext.Provider>;
+  const [num, setNum] = useState(0);
+  return <FirstContext.Provider value={{ userName, address, num, setNum }}>{props.children}</FirstContext.Provider>;
 };
 
 export default FirstContextProvider;
--- src/components/Child3.jsx
+++ src/components/Child3.jsx
@@ -2,11 +2,14 @@ import { useContext } from "react";
 import { FirstContext } from "../FirstContext";
 
 const Child3 = () => {
-  const { userName } = useContext(FirstContext);
+  const { userName, num, setNum } = useContext(FirstContext);
   return (
     <>
       <h1>Child3</h1>
       <h2>Data from FirstContext username - {userName}</h2>
+      <h1>{num}</h1>
+      <button onClick={() => setNum((prev) => prev + 1)}>+</button>
+      <button onClick={() => setNum((prev) => prev - 1)}>-</button>
     </>
   );
 };
